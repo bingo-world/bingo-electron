eb2ea4c7b1d35cdef4a1f012c0f178d9956dbcc0
Bart Wijnants <bartwijnants@outlook.com>
2016-12-17 15:03:35 +0100

    linting / travis / electron builder

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow} = require('electron');
 const url = require('url');
+const path = require('path');
 
 let win;
 
@@ -7,7 +8,7 @@ function createWindow() {
     win = new BrowserWindow({ width: 800, height: 600 });
 
     win.loadURL(url.format({
-        pathname: 'index.html',
+        pathname: path.join(__dirname, 'index.html'),
         protocol: 'file:',
         slashes: true
     }));
@@ -15,7 +16,7 @@ function createWindow() {
     win.webContents.openDevTools();
 
     win.on('closed', () => {
-        win = null
+        win = null;
     });
 }
 
